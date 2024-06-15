211d8e346af0952ca4042f7bbb704b0f11c84102
jbui184 <johnnybui184@gmail.com>
2024-06-15 14:54:14 +1000

    part 1 completed

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {'finches': finches})
+
+
+
+
+
+finches = [
+    {'name': 'Alpha', 'breed': 'Red-eared Firetail', 'description':'trying to be Charizard with my eyebrows on fleek', 'age': 1},
+    {'name': 'Beta', 'breed': 'Gouldian', 'description':'jaundice faced with purple honkers', 'age': 2},
+    {'name': 'Gamma', 'breed': 'Double-barred', 'description':'I am the owl, sir hold my cloak', 'age': 3},
+    {'name': 'Delta', 'breed': 'Zebra', 'description':'Oh, Sugar Honey Ice Tea', 'age': 4},
+    {'name': 'Epsilon', 'breed': 'Beautiful Firetail', 'description':'glamourous charizard, no eyeshadow', 'age': 5},
+]
