24ed902f38bb8a2244fbd290e1954b5572670ad2
jbui184 <johnnybui184@gmail.com>
2024-06-19 12:59:07 +1000

    part 3 of finches lab

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -9,3 +10,6 @@ class Finch(models.Model):
     age = models.IntegerField()
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwarg={'finch_id': self.id})
